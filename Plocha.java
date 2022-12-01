f081f7fa537a0e7153388c35c0445d084282860c
keramare <kerata.marek1@gmail.com>
2022-12-01 23:33:39 +0100

    pridanie vykreslenia plochy + vytvorenie bomby na suradniciach

--- Plocha.java
+++ Plocha.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 /**
  * Write a description of class Plocha here.
  * 
@@ -6,10 +6,40 @@
  * @version (a version number or a date)
  */
 public class Plocha {
-    /**
-     * Constructor for objects of class Plocha
-     */
-    public Plocha() {
-        // initialise instance variables
+    Policko[][] zoznamPolicok;
+    private int riadky;
+    private int stlpce;
+    public Plocha(int riadky, int stlpce) {
+        zoznamPolicok = new Policko[riadky][stlpce];
+        vytvorPlochu(riadky, stlpce);
     }
+    
+    public void vytvorPlochu(int riadky, int stlpce){
+        this.riadky = riadky;
+        this.stlpce = stlpce;
+        for (int i = 0; i < riadky; i++){
+            for (int j = 0; j < riadky; j++){
+                zoznamPolicok[i][j] = new Policko(i, j);
+                zoznamPolicok[i][j].vykresli();
+            }
+            System.out.println("");
+        }
+    }
+    
+    public void updatePlocha(int riadky, int stlpce){
+        for (int i = 0; i < riadky; i++){
+            for (int j = 0; j < riadky; j++){
+                zoznamPolicok[i][j].vykresli();
+            }
+            System.out.println("");
+        }
+    }
+    
+    public void vytvorBombu(int x, int y){
+        zoznamPolicok[x][y].vytvorBombu();
+        System.out.println("----------------------------------");
+        System.out.println("");
+        updatePlocha(this.riadky, this.stlpce);
+    }
+    
 }
---
+++ Policko.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class mina here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Policko {
+    private int x;
+    private int y;
+    private boolean jeBomba = false;
+    public Policko(int y, int x) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX(){
+        return this.x;
+    }
+    
+    public int getY(){
+        return this.y;
+    }
+    
+    public boolean getJeBomba(){
+        return this.jeBomba;
+    }
+    
+    public void vytvorBombu(){
+        this.jeBomba = true;
+    }
+    
+    public void vykresli(){
+        if (this.getJeBomba() == true) {
+            System.out.print(" * ");
+        }
+        else {
+            System.out.print(" 0 ");
+        }
+    }
+}
