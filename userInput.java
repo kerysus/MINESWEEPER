608c2aa5eed985c292b0e9c7a365ade0f128a81d
Marek Kerata <kerata.marek1@gmail.com>
2022-12-06 20:07:43 +0100

    upravenie user inputu

--- userInput.java
+++ userInput.java
@@ -1,32 +1,21 @@
 import java.util.Scanner;
 public class userInput {
-
-    public userInput() {
-        
-    }
+    private Plocha plocha;
     
-    public Plocha vytvorPlochu(){
+    public userInput() {
+        this.vytvorPlochu();
+    }
+
+    public void vytvorPlochu(){
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Pocet riadkov: "); 
         int riadky = scanner.nextInt(); 
-        
+
         System.out.println("Pocet stlpcov: "); 
         int stlpce = scanner.nextInt(); 
         
-        Plocha plocha1 = new Plocha(riadky, stlpce);
-        return plocha1;
-    }
-    
-    public Plocha makeAGuess(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("X: "); 
-        int x = scanner.nextInt(); 
-        
-        System.out.println("Y: "); 
-        int y = scanner.nextInt(); 
-        
+        this.plocha = new Plocha(riadky, stlpce);
         
     }
 }
