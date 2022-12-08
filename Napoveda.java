fa801b946bcbf026e8a2c4f79420355b5c9e0e82
MajsterMari <gazur@stud.uniza.sk>
2022-12-08 18:45:05 +0100

    pridanie loga s napovedou

--- Napoveda.java
+++ Napoveda.java
@@ -1,13 +1,33 @@
-
 public class Napoveda {
 
     public Napoveda() {
 
     }
     public void dajNapovedu(){
+        System.out.println("                              \\\\   //");
+        System.out.println("                               \\\\_//");
+        System.out.println("                                \\_/");
+        System.out.println("  _________________     ___________________    ________________");
+        System.out.println(" /_______________ ||   ||_______   _______||  ||  __________  \\\\");
+        System.out.println("              // //            || ||          || ||         \\\\ \\\\");
+        System.out.println("             // //             || ||          || ||          \\\\ \\\\");
+        System.out.println("            // //              || ||          || ||          || ||");
+        System.out.println("           // //               || ||          || ||         // //");
+        System.out.println("          // //                || ||          || ||        // //");
+        System.out.println("         // //                 || ||          || ||_______// //");
+        System.out.println(" _______// //______            || ||          ||   _________//");
+        System.out.println("/_______    ______/            || ||          || ||");
+        System.out.println("       // //                   || ||          || ||");
+        System.out.println("      // //                    || ||          || ||");
+        System.out.println("     // //                     || ||          || ||");
+        System.out.println("    // //                      || ||          || ||");
+        System.out.println("   // //                       || ||          || ||");
+        System.out.println("  // //                        || ||          || ||");
+        System.out.println(" // //                         || ||          || ||");
+        System.out.println("// //______________            ||_||          ||_||");
+        System.out.println("||________________/            |___|          |___|");
+        System.out.println("");
         System.out.println("Hra sa hrá zadávaním dvoch čísiel ktoré slúžia ako číslo riadku a číslo stĺpca, ktorý si želáte označiť.");
         System.out.println("Ak sa vo Vami zvolenom políčku nachádza mína, hru prehrávate.");
-        
-        
     }
 }
