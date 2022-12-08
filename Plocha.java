2cc3a5239b643e8aea4b012ca4d46ab54a317953
Marek Kerata <kerata.marek1@gmail.com>
2022-12-08 18:03:34 +0100

    opravenie bugov (opravena skalovatelnost pri plocha viac ako 10, opravenie bugu pri vykreslovani hodnot prazdnuch policok)

--- Plocha.java
+++ Plocha.java
@@ -59,12 +59,17 @@ public class Plocha {
     public void updatePlocha(int riadky, int stlpce){
         System.out.print("   ");
         for (int x = 1; x <= stlpce; x++){
-            System.out.print("  " + x + "  ");
+            if (x > 9){
+                System.out.print(" " + x + "  ");
+            }
+            else{
+                System.out.print("  " + x + "  ");
+            }
         }
         System.out.println("");
         System.out.print("   ");
         for (int j = 1; j <= stlpce; j++){
-            System.out.print("=====");
+            System.out.print("======");
         }
         System.out.println("");
         
@@ -75,12 +80,18 @@ public class Plocha {
             else{
                 System.out.print((i+1) + "|");
             }
+            
+            
             for (int j = 0; j < riadky; j++){
-                zoznamPolicok[i][j].vykresli();
+                    zoznamPolicok[i][j].vykresli();     
             }
             System.out.println("");
         }
-        System.out.println("=====================================================");
+        
+        for (int j = 1; j <= stlpce; j++){
+            System.out.print("======");
+        }
+        System.out.println("");
     }
     
     //vytvori minu na danej suradnici
@@ -126,22 +137,25 @@ public class Plocha {
                 for (int policko = 0; policko < 3; policko++){
                     int index1 = (x-2)+riadok;
                     int index2 = (y-2)+policko;
-                    if ((index1 > -1) && (index2 > -1)){
+                    if ((index1 == -1 || index2 == -1) || (index1 == this.zoznamPolicok[x-1].length || index2 == this.zoznamPolicok.length)){
+                        continue;
+                    }
+                    else{
                         if (zoznamPolicok[index1][index2].getJeBomba()){
                             pocetMinOkolo++;
                         }
                         else{
                             this.setBombsInArea(x, y, pocetMinOkolo);
                         }
-                    }
+                    } 
                 }
             }
         return pocetMinOkolo;
     }
     
     public void zistiPocetMinOkoloVsetkychPolicok(){
-        for (int riadok = 0; riadok < this.zoznamPolicok.length-1; riadok++){
-            for (int prvok = 0; prvok < this.zoznamPolicok[riadok].length-1; prvok++){   
+        for (int riadok = 0; riadok < this.zoznamPolicok.length; riadok++){
+            for (int prvok = 0; prvok < this.zoznamPolicok[riadok].length; prvok++){   
                 this.zoznamPolicok[riadok][prvok].setBombsInArea(this.zistiPocetMinOkolo(riadok+1, prvok+1));
             }
         }
