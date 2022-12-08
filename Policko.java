1f9f2f9bd079225dc41324eb6e2ab202c94caba4
Marioy <117741675+maryoxd@users.noreply.github.com>
2022-12-08 19:38:04 +0100

    Pridanie súborov

---
+++ Policko.java
@@ -0,0 +1,113 @@
+
+/**
+ * Write a description of class mina here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Policko {
+    private int x;
+    private int y;
+    private int bombsInArea;
+    private boolean jeBomba;
+    private boolean jeVedlaBomba;
+    private boolean jeOdhalena;
+    private boolean maVlajku;
+    private boolean obsadene;
+
+    public Policko(int y, int x) {
+        this.x = x;
+        this.y = y;
+        this.jeOdhalena = false;
+        this.jeVedlaBomba = false;
+        this.jeBomba = false;
+        this.maVlajku = false;
+        this.obsadene = false;
+    }
+    
+    public int getX(){
+        return this.x;
+    }
+    
+    public int getY(){
+        return this.y;
+    }
+    
+    public int getBombsInArea(){
+        return this.bombsInArea;
+    }
+    
+    public boolean getJeBomba(){
+        return this.jeBomba;
+    }
+    
+    public boolean getJeObsadene(){
+        return this.obsadene;
+    }
+    
+    public boolean getMaVlajku(){
+        return this.maVlajku;
+    }
+    
+    public void setJeObsadene(boolean hodnota){
+        this.obsadene = hodnota;
+    }
+
+    public void setMaVlajku(boolean hodnota){
+        this.maVlajku = hodnota;
+    }
+    
+    public void setJeBomba(boolean hodnota){
+        this.jeBomba = hodnota;
+    }
+    
+    public void setJeOdhalena(boolean hodnota){
+        this.jeOdhalena = hodnota;
+    }
+    
+    public void setBombsInArea(int pocet){
+        this.bombsInArea = pocet;
+    }
+    
+    public boolean makeGuess(int x, int y){
+        return false;
+    }
+    
+    public void vytvorBombu(){
+        this.jeBomba = true;
+    }
+    
+    //vykresli dane policko podla toho aky ma stav
+    public void vykresli(){
+        if (this.jeOdhalena){
+            if (this.bombsInArea==0){
+                System.out.print("     ");
+                this.obsadene = true;
+            }
+            else {
+                System.out.print(" |" + this.bombsInArea + "| ");
+            }
+        }     
+        
+        else if (this.maVlajku){
+            if (this.bombsInArea==0){
+                this.obsadene = true;
+            }
+                System.out.print(" |P| ");
+            }
+        
+        else {
+            if (this.bombsInArea==0){
+                //System.out.print("     ");
+                this.obsadene = true;
+            }
+            
+            if (this.jeBomba){
+                System.out.print(" |*| ");
+            }
+            else{
+                System.out.print(" |0| ");
+            }
+            }
+        }
+}
---
+++ userInput.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class userInput {
+    private Plocha plocha;
+    private Scanner scanner;
+    
+    public userInput() {
+        this.scanner = new Scanner(System.in);
+        this.vytvorPlochu();
+        this.vytvorPoleMin();
+        this.makeGuesses();
+    }
+
+    public void vytvorPlochu(){
+        System.out.println("Zadaj rozmer plochy: "); 
+        int riadky = scanner.nextInt(); 
+        int stlpce = riadky;
+        this.plocha = new Plocha(riadky, stlpce);
+    }
+    
+    public void vytvorPoleMin(){
+        System.out.println("Zadaj pocet min: ");
+        int pocetMin = scanner.nextInt();
+        this.plocha.vytvorPoleBomb(pocetMin);
+    }
+    
+    public void makeGuesses(){
+        if(!this.plocha.getVyhra() && !this.plocha.getPrehra()){
+            System.out.println("Vyber: odhad(1), poloz vlajku(2): ");
+            int vyber = scanner.nextInt();
+            
+            if (vyber==1){
+                System.out.println("Zadaj X: ");
+                int suradnicaX = scanner.nextInt();
+                
+                System.out.println("Zadaj Y: ");
+                int suradnicaY = scanner.nextInt();
+                this.plocha.makeGuess(suradnicaY, suradnicaX);
+            }
+            else{
+                System.out.println("Zadaj X: ");
+                int suradnicaX = scanner.nextInt();
+                
+                System.out.println("Zadaj Y: ");
+                int suradnicaY = scanner.nextInt();
+                this.plocha.polozVlajku(suradnicaY, suradnicaX);
+            }
+            this.makeGuesses();
+        }
+    }
+}
