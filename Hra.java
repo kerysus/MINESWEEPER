5a4d13b6fd754027cdedda216fe09b6f93df8835
Keramare <92203484+keramare@users.noreply.github.com>
2022-12-12 20:02:53 +0100

    dokumentácia triedy Hra()

--- Hra.java
+++ Hra.java
@@ -1,5 +1,11 @@
+/**
+ * Táto trieda spúšťa samotnú hru.
+ */
 public class Hra {
     private UserInput userInput;
+    /**
+     * Táto metóda tvorí konštruktor triedy, v ktorej volá len jednu metódu na spustenie hry.
+     */
     public Hra() {
         this.userInput = new UserInput();
     }
